dd7f485169380be79016a72e7b198a3d8a6a2c3e
mundaugame <mundaugame@gmail.com>
2020-10-04 10:59:16 -0300

    Updates

---
+++ precache-manifest.4b97f36d68a3bdf77f22a9d37b2db5c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1720f3ddf6cb176210aa709ff4332183",
+    "url": "/monsters-rolodex-page/index.html"
+  },
+  {
+    "revision": "789388c949631fc89643",
+    "url": "/monsters-rolodex-page/static/css/main.d1daefa1.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex-page/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex-page/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "789388c949631fc89643",
+    "url": "/monsters-rolodex-page/static/js/main.d8325edd.chunk.js"
+  },
+  {
+    "revision": "9a423bf45f057c4001a7",
+    "url": "/monsters-rolodex-page/static/js/runtime-main.878f4144.js"
+  }
+]);
